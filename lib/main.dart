2ba6ab6c74dc3ce62898ef75dd1c86f07b2c2096
riteshpn <prasadritesh261@gmail.com>
2023-12-20 16:32:08 +0530

    add firebase and add packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:assigment_project/firebase_options.dart';
 import 'package:assigment_project/screens/welocme_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options:DefaultFirebaseOptions.currentPlatform );
   runApp(const MyApp());
 }
 
