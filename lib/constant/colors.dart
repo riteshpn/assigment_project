9b76b6cb2c8e5b47b30b24d3b6c412d323750aa4
riteshpn <prasadritesh261@gmail.com>
2023-12-20 12:46:16 +0530

    add constant file for colors&textstyle

---
+++ lib/constant/colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ColorsConstant{
+    static const lightBlue = Colors.lightBlue; // Replace with your primary color code
+    static const darkBlue = Color.fromARGB(255, 36, 133, 212); // Replace with your secondary color code
+
+  // Text styles
+  static const TextStyle headingTextStyle = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+  static const TextStyle headingTextStyle2 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.normal,
+    color: Colors.white,
+  );
+
+  static const TextStyle bodyTextStyle = TextStyle(
+    fontSize: 16,
+    color: Colors.grey,
+  );
+
+}
