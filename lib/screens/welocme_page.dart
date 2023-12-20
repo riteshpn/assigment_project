48af2e6ffe96855cda90a0842fd715aa6dd149c9
riteshpn <prasadritesh261@gmail.com>
2023-12-20 13:39:10 +0530

    media querry add in welcome screen

--- lib/screens/welocme_page.dart
+++ lib/screens/welocme_page.dart
@@ -11,22 +11,45 @@ class WelcomePage extends StatefulWidget {
 class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
-    CommonConstant.setMediaQuery(context);
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.only(top: ),
+        padding: EdgeInsets.only(top: screenHeight * 0.2),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Icon(
               Icons.home,
               color: CommonConstant.lightBlue,
-              size: CommonConstant.screenWidth! * 0.25,
+              size: screenWidth * 0.25,
             ),
             const Text(
               'Apna Ghar',
               style: CommonConstant.blueheadingTextStyle,
-            )
+            ),
+            SizedBox(
+              height: screenHeight * 0.03,
+            ),
+            const Center(
+              child: Column(
+                children: [
+                  Text(
+                    'For Every Home problems we',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  Text(
+                    'have solution',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: screenHeight * 0.08,
+            ),
+            Text('Sign Up', style: CommonConstant.black),
           ],
         ),
       ),
